8b8f216f093281bcc0b4aac85cb59c8de8daa309
carlk <carloskachuk@gmail.com>
2022-05-03 02:15:46 -0300

    project done

--- routes/index.js
+++ routes/index.js
@@ -24,7 +24,9 @@ router.get('/new', function(req, res, next) {
 });
 
 router.post('/new', function(req, res, next){
-  
+  messages.push({text: req.body.message, user: req.body.author, added: new Date()});
+
+  res.redirect('/');
 })
 
 module.exports = router;
